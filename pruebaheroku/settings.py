5e02d27f4e3845965a8a59ddf72737dcc6c14e6d
dafma <mar_tp@hotmail.es>
2020-04-29 17:00:53 -0500

    otro

--- pruebaheroku/settings.py
+++ pruebaheroku/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '8=hjxgvgea98o@*x%g*&9$1q4_7n#0$$@_@nevwy42!vk-3eot'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["librosproject.herokuapp.com"]
 
 
 # Application definition
