c56c795d6c2cd7ca56474fc5d974ac22f02aa243
dafma <mar_tp@hotmail.es>
2020-04-29 16:28:19 -0500

    reads

--- pruebaheroku/settings.py
+++ pruebaheroku/settings.py
@@ -75,14 +75,13 @@ WSGI_APPLICATION = 'pruebaheroku.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
+
+import dj_database_url 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
-
-
+    'default': dj_database_url.config(
+        default=config('DATABASE_URL')
+    )
+}s
 # Password validation
 # https://docs.djangoproject.com/en/3.0/ref/settings/#auth-password-validators
 
