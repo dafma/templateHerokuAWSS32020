129bb776cf8f3e99cfd1d342f7dbc394784444ca
dafma <mar_tp@hotmail.es>
2020-05-01 11:02:06 -0500

    rtfg

--- app/models.py
+++ app/models.py
@@ -2,8 +2,8 @@ from django.db import models
 
 # Create your models here.
 class Perfil(models.Model):
-	titulo = models.CharField(max_length=200, null=True)
-	foto = models.ImageField(default="profile1.png", null=True, blank=True)
+    titulo = models.CharField(max_length=200, null=True)
+    foto = models.ImageField(default="profile1.png", null=True, blank=True)
 
     def __str__(self):
         return self.titulo
